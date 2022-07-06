abd682c68f5147f8876130f115687d87e765c1f2
Say Cheong <greymistcube@gmail.com>
2022-07-06 16:28:42 +0900

    Basic PlayerState methods
    ResetPlayer method added

--- Assets/_Scripts/States/PlayerState.cs
+++ Assets/_Scripts/States/PlayerState.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Contracts;
+using Libplanet;
 using Libplanet.Store;
 
 namespace Flawless.States
@@ -7,19 +9,43 @@ namespace Flawless.States
     /// </summary>
     public class PlayerState : DataModel
     {
+        public const long InitialGold = 0;
+
+        public string Name { get; private set; }
+        public Address Address { get; private set; }
         public StatsState StatsState { get; private set; }
         public WeaponState WeaponState { get; private set; }
+        public long Gold { get; private set; }
         public BestRecordState BestRecordState { get; private set; }
 
         /// <summary>
         /// Creates a new <see cref="PlayerState"/> instance.
         /// </summary>
-        public PlayerState()
+        public PlayerState(string name, Address address)
             : base()
         {
+            Name = name;
+            Address = address;
             StatsState = new StatsState();
             WeaponState = new WeaponState();
+            Gold = InitialGold;
             BestRecordState = new BestRecordState();
+        }
+
+        private PlayerState(
+            string name,
+            Address address,
+            StatsState statsState,
+            WeaponState weaponState,
+            long gold,
+            BestRecordState bestRecordState)
+        {
+            Name = name;
+            Address = address;
+            StatsState = statsState;
+            WeaponState = weaponState;
+            Gold = gold;
+            BestRecordState = bestRecordState;
         }
 
         /// <summary>
@@ -30,6 +56,78 @@ namespace Flawless.States
         public PlayerState(Bencodex.Types.Dictionary encoded)
             : base(encoded)
         {
+        }
+
+        [Pure]
+        public PlayerState UpdateStats(StatsState statsState)
+        {
+            return new PlayerState(
+                name: Name,
+                address: Address,
+                statsState: statsState,
+                weaponState: WeaponState,
+                gold: Gold,
+                bestRecordState: BestRecordState);
+        }
+
+        [Pure]
+        public PlayerState UpgradeWeapon(WeaponState weaponState)
+        {
+            return new PlayerState(
+                name: Name,
+                address: Address,
+                statsState: StatsState,
+                weaponState: weaponState,
+                gold: Gold,
+                bestRecordState: BestRecordState);
+        }
+
+        [Pure]
+        public PlayerState SellWeapon()
+        {
+            return new PlayerState(
+                name: Name,
+                address: Address,
+                statsState: StatsState,
+                weaponState: new WeaponState(),
+                gold: Gold + WeaponState.GetPrice(),
+                bestRecordState: BestRecordState);
+        }
+
+        [Pure]
+        public PlayerState AddGold(long gold)
+        {
+            return new PlayerState(
+                name: Name,
+                address: Address,
+                statsState: StatsState,
+                weaponState: WeaponState,
+                gold: Gold + gold,
+                bestRecordState: BestRecordState);
+        }
+
+        [Pure]
+        public PlayerState UpdateBestRecord(BestRecordState bestRecordState)
+        {
+            return new PlayerState(
+                name: Name,
+                address: Address,
+                statsState: StatsState,
+                weaponState: WeaponState,
+                gold: Gold,
+                bestRecordState: bestRecordState);
+        }
+
+        [Pure]
+        public PlayerState ResetPlayer()
+        {
+            return new PlayerState(
+                name: Name,
+                address: Address,
+                statsState: new StatsState(),
+                weaponState: new WeaponState(),
+                gold: InitialGold,
+                bestRecordState: BestRecordState);
         }
     }
 }
